a439367b9294df7380a487c2d3be370370908d76
Johnny77585 <jkuo2188@gmail.com>
2023-02-06 15:00:48 +0800

    feat: passport init & signin

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -8,16 +8,33 @@ const userController = {
   signUp: (req, res, next) => {
     if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
     User.findOne({ where: { email: req.body.email } })
-    return bcrypt.hash(req.body.password, 10)
+      .then(user => {
+        if (user) throw new Error('Email already exists!')
+        return bcrypt.hash(req.body.password, 10)
+      })
       .then(hash => User.create({
         name: req.body.name,
         email: req.body.email,
         password: hash
       }))
       .then(() => {
-        req.flash('success_message', '成功註冊帳號!')
+        req.flash('success_messages', '成功註冊帳號！')
         res.redirect('/signin')
       })
+      .catch(err => next(err))
+  },
+  signInPage: (req, res) => {
+    res.render('signin')
+  },
+  signIn: (req, res) => {
+    req.flash('success_messages', '成功登入！')
+    res.redirect('/restaurants')
+  },
+  logout: (req, res) => {
+    req.flash('success_messages', '登出成功！')
+    req.logout()
+    res.redirect('/signin')
   }
 }
+
 module.exports = userController
