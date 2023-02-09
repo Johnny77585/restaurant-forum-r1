8f50c40f95c9a26a08ad34f371232f3e26abacf8
Johnny77585 <jkuo2188@gmail.com>
2023-02-09 12:39:42 +0800

    feat: modify admin restaurants page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,5 +3,5 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 const { authenticatedAdmin } = require('../../middleware/auth')
 router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', (req, res) => res.redirect('admin/restaurants'))
 module.exports = router
