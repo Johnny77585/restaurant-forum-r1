b7d52746df0d795d678ae26e66dfd1a8ffb9b3e2
Johnny77585 <jkuo2188@gmail.com>
2023-02-25 14:53:41 +0800

    feat: category create

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -5,6 +5,23 @@ const categoryController = {
       raw: true
     })
       .then(categories => res.render('admin/categories', { categories }))
+  },
+  postCategories: (req, res, next) => {
+    const { name } = req.body
+    if (!name) {
+      throw new Error('Category name is required!')
+    }
+    Category.findOne({ where: { name } })
+      .then(category => {
+        if (category) {
+          throw new Error('Category name already exists!')
+        }
+        return Category.create({ name })
+      })
+      .then(() => {
+        res.redirect('/admin/categories')
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = categoryController
