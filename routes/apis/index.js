792eb19ee036c4c0c590dbf5f350c2687c4fd072
Johnny77585 <jkuo2188@gmail.com>
2023-03-26 13:41:09 +0800

    feat: api signin add jwt token

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const restController = require('../../controllers/apis/restaurant-controller')
+const passport = require('../../config/passport')
+const userController = require('../../controllers/apis/user-controller')
 
+router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
 router.get('/restaurants', restController.getRestaurants)
 module.exports = router
