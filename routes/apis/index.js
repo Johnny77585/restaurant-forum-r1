2c6df1f5b6017aeb696d21bca4decefbeca32cd6
Johnny77585 <jkuo2188@gmail.com>
2023-03-26 14:25:25 +0800

    feat: add admin-services

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const admin = require('./modules/admin')
 const restController = require('../../controllers/apis/restaurant-controller')
 const passport = require('../../config/passport')
 const userController = require('../../controllers/apis/user-controller')
 
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
+router.use('/admin', admin)
 router.get('/restaurants', restController.getRestaurants)
 module.exports = router
