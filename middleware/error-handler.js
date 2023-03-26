9d104cd702cdf2fa703782550fabd5304af0e8da
Johnny77585 <jkuo2188@gmail.com>
2023-03-26 15:05:17 +0800

    feat: add api error handler & delete restaurant api

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -6,7 +6,20 @@ module.exports = {
       req.flash('error_messages', `${err}`)
     }
     res.redirect('back')
-
+    next(err)
+  },
+  apiErrorHandler (err, req, res, next) {
+    if (err instanceof Error) {
+      res.status(err.status || 500).json({
+        status: 'error',
+        message: `${err.name}: ${err.message}`
+      })
+    } else {
+      res.status(500).json({
+        status: 'error',
+        message: `${err}`
+      })
+    }
     next(err)
   }
 }
--- routes/apis/index.js
+++ routes/apis/index.js
@@ -2,10 +2,12 @@ const express = require('express')
 const router = express.Router()
 const admin = require('./modules/admin')
 const restController = require('../../controllers/apis/restaurant-controller')
+const { apiErrorHandler } = require('../../middleware/error-handler')
 const passport = require('../../config/passport')
 const userController = require('../../controllers/apis/user-controller')
 
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
 router.use('/admin', admin)
 router.get('/restaurants', restController.getRestaurants)
+router.use('/', apiErrorHandler)
 module.exports = router
